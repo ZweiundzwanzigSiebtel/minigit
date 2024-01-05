111f0af3dc663b56638385f0dae44afa21abcb72
Silas Ruffner <silas.ruffner@outlook.com>
2024-01-05 09:32:54 +0100

    implement hash-object() function

--- minigit/data.py
+++ minigit/data.py
@@ -1,6 +1,22 @@
 import os
+import hashlib
 
 GIT_DIR = ".minigit"
 
 def init():
     os.makedirs(GIT_DIR)
+    os.makedirs(f'{GIT_DIR}/objects')
+
+def hash_object(data):
+    __check_minigit_dir()
+    oid = hashlib.sha1(data).hexdigest()
+    print("oid is: ", oid)
+    print(f'{GIT_DIR}(objects/{oid})')
+    with open(f'{GIT_DIR}/objects/{oid}', 'wb') as out_file:
+        out_file.write(data)
+    return oid
+
+
+def __check_minigit_dir():
+    #check if a .minigit dir is present
+    pass
