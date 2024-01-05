905127f27f01460d195e982b08a28ce784004c78
Silas Ruffner <silas.ruffner@outlook.com>
2024-01-05 09:30:45 +0100

    use setuptools instead of PyInstaller

--- minigit/main.py
+++ minigit/main.py
@@ -16,12 +16,23 @@ def parse_args():
     init_parser = commands.add_parser('init')
     init_parser.set_defaults(func=init)
 
+    hash_object_parser = commands.add_parser ('hash-object')
+    hash_object_parser.set_defaults (func=hash_object)
+    hash_object_parser.add_argument ('file')
+
     return parser.parse_args()
+
 
 def init(args):
     data.init()
     print(f"Initialized empty minigit repository in {os.getcwd()}/{data.GIT_DIR}")
 
-if __name__ == "__main__":
-    main()
 
+def hash_object(args):
+    with open(args.file, 'rb') as file:
+        print(data.hash_object(file.read()))
+
+
+if __name__ == '__main__':
+    print("start programm...")
+    main()
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+from setuptools import setup
+
+setup(
+    name='minigit',
+    version='0.1',
+    packages=['minigit'],
+    entry_points={
+        'console_scripts': [
+            'minigit=minigit.main:main',  # Beachte den Doppelpunkt und die Funktion
+        ],
+    },
+)
+
